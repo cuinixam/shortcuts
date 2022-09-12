d456dc49d83f483c31bda3d3378b0604a397b750
Karsten A. M. Guenther <karsten.guenther@kamg.de>
2022-09-12 21:16:37 +0200

    initial commit

---
+++ build.ps1
@@ -0,0 +1,92 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', '', Justification = 'Used in global scope in Main function')]
+param(
+    [switch]$clean ## clean build, wipe out all build artifacts
+    , [switch]$install ## install mandatory packages
+)
+
+Function Invoke-CommandLine {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingInvokeExpression', '', Justification = 'Usually this statement must be avoided (https://learn.microsoft.com/en-us/powershell/scripting/learn/deep-dives/avoid-using-invoke-expression?view=powershell-7.3), here it is OK as it does not execute unknown code.')]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$CommandLine,
+        [Parameter(Mandatory = $false, Position = 1)]
+        [bool]$StopAtError = $true,
+        [Parameter(Mandatory = $false, Position = 2)]
+        [bool]$Silent = $false
+    )
+    if (-Not $Silent) {
+        Write-Output "Executing: $CommandLine"
+    }
+    $global:LASTEXITCODE = 0
+    Invoke-Expression $CommandLine
+    if ($global:LASTEXITCODE -ne 0) {
+        if ($StopAtError) {
+            Write-Error "Command line call `"$CommandLine`" failed with exit code $global:LASTEXITCODE"
+        }
+        else {
+            if (-Not $Silent) {
+                Write-Output "Command line call `"$CommandLine`" failed with exit code $global:LASTEXITCODE, continuing ..."
+            }
+        }
+    }
+}
+
+Function Import-Dot-Env {
+    if (Test-Path -Path '.env') {
+        # load environment properties
+        $envProps = ConvertFrom-StringData (Get-Content '.env' -raw)
+    }
+
+    Return $envProps
+}
+
+Function Initialize-Proxy {
+    $envProps = Import-Dot-Env
+    if ($envProps.'HTTP_PROXY') {
+        $Env:HTTP_PROXY = $envProps.'HTTP_PROXY'
+        $Env:HTTPS_PROXY = $Env:HTTP_PROXY
+        if ($envProps.'NO_PROXY') {
+            $Env:NO_PROXY = $envProps.'NO_PROXY'
+            $WebProxy = New-Object System.Net.WebProxy($Env:HTTP_PROXY, $true, ($Env:NO_PROXY).split(','))
+        }
+        else {
+            $WebProxy = New-Object System.Net.WebProxy($Env:HTTP_PROXY, $true)
+        }
+
+        [net.webrequest]::defaultwebproxy = $WebProxy
+        [net.webrequest]::defaultwebproxy.credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+    }
+}
+
+Function Main {
+    Push-Location $PSScriptRoot
+    Write-Output "Running in ${pwd}"
+    
+    Initialize-Proxy
+    
+    if ($install) {
+        if (-Not (Test-Path -Path '.bootstrap')) {
+            New-Item -ItemType Directory '.bootstrap'
+        }
+        # Installation of Scoop, Python and pipenv via bootstrap
+        $bootstrapSource = 'https://raw.githubusercontent.com/avengineers/bootstrap/develop/bootstrap.ps1'
+        if ($Env:GITHUB_HEAD_REF){
+            $bootstrapSource = "https://raw.githubusercontent.com/avengineers/bootstrap/$Env:GITHUB_HEAD_REF/bootstrap.ps1"
+            Write-Output "Downloading bootstrap from $bootstrapSource ..."
+        }
+        Invoke-RestMethod $bootstrapSource -OutFile '.\.bootstrap\bootstrap.ps1'
+        Invoke-CommandLine '. .\.bootstrap\bootstrap.ps1' -Silent $true
+        Write-Output "For installation changes to take effect, please close and re-open your current shell."
+    }
+    else {
+        # Do nothing
+    }
+
+    Pop-Location
+}
+
+## start of script
+$ErrorActionPreference = "Stop"
+Main
+## end of script
+
